dd10bec62fb8ab9257326577d4bd6a04cde935bb
renatoBueno <renato.bueno@performait.com>
2026-02-20 11:39:50 -0300

    update base URL in vite.config.ts to serve from /mariaflores/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // Updated to serve at root URL
+  base: "/mariaflores/",
   server: {
     host: "::",
     port: 8080,
