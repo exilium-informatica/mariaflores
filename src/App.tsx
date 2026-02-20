76767ad728ec8d2c6350bf3ec6504f71e941ce15
renatoBueno <renatosapa@gmail.com>
2026-02-20 12:07:17 -0300

    update BrowserRouter to include basename for proper routing

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/mariaflores/">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
